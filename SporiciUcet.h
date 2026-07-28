968cf77db8d90666c4d84749f37c848635db9c10
cabrnochjan <xxx.cabrnoch.jan.xxx@gmail.com>
2026-07-28 20:16:57 +0200

    Update SporiciUcet.h

--- SporiciUcet.h
+++ SporiciUcet.h
@@ -1,16 +1,1 @@
-#pragma once
-#include "Ucet.h"
-#include <string>
-#include <vector>
-#include <iostream>
-using namespace std;
 
-class SporiciUcet : public Ucet
-{
-    private: 
-        double urokovaSazba;
-    public:
-        SporiciUcet(string cisloUctu, double urokovaSazba);
-        void vypisInfo()const override;
-        void analyzujUcet()const override;
-};
